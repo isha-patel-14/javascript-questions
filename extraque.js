24521ebb5bec902d71ac0bf0c3962cd1a7a5f45c
isha-patel-14 <isha.patel.cg@gmail.com>
2024-08-28 23:19:29 +0530

    extraque.js

--- extraque.js
+++ extraque.js
@@ -9,22 +9,22 @@
 
 
 //question-missing element
-function findMissing(arr, N) {
-    let i;
-    let temp = new Array(N).fill(0);
-    for (i = 0; i < N; i++) {
-        temp[arr[i] - 1] = 1;
-    }
-    let ans = 0;
-    for (i = 0; i <= N; i++) {
-        if (temp[i] === 0)
-            ans = i + 1;
-    }
-    console.log(ans);
-}
-let arr = [1,3,2,4,5,7];
-let n = arr.length;
-findMissing(arr, n);
+// function findMissing(arr, N) {
+//     let i;
+//     let temp = new Array(N).fill(0);
+//     for (i = 0; i < N; i++) {
+//         temp[arr[i] - 1] = 1;
+//     }
+//     let ans = 0;
+//     for (i = 0; i <= N; i++) {
+//         if (temp[i] === 0)
+//             ans = i + 1;
+//     }
+//     console.log(ans);
+// }
+// let arr = [1,3,2,4,5,7];
+// let n = arr.length;
+// findMissing(arr, n);
 
 
 //question-sorting without sort function 
